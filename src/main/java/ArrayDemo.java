450cce2283cb2273d7ece92752abe6cc60cac8f9
Elena <kochkurova.en@gmail.com>
2023-02-18 18:49:14 +0600

    Массивы 5я глава

---
+++ src/main/java/ArrayDemo.java
@@ -0,0 +1,32 @@
+public class ArrayDemo {
+    public static void main (String args[]) {
+        int sample[] = new int[10];
+        int i;
+         for (i = 0; i < 10; i ++ )
+             sample [i] = i;
+
+         for (i = 0; i < 10; i ++)
+            System.out.println("Элемент [" + i + "]: " + sample [i]);
+
+         System.out.println();
+
+
+         int example[] = new int[10];
+         int z;
+
+         for (z = 0; z < 10; z ++)
+             example [z] = z;
+
+         for (z = 0; z < 10; z ++)
+             System.out.println("Элемент [" + z + "]: " + sample [z]);
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/MinMax.java
@@ -0,0 +1,38 @@
+public class MinMax {
+    public static void main (String args [])
+    {
+        int nums [] = new int[10];
+        int min, max;
+        nums[0] = 99;
+        nums[1] = -100;
+        nums[2] = 88;
+        nums[3] = 33;
+        nums[4] = -144;
+        nums[5] = 789;
+        nums[6] = 285;
+        nums[7] = 344;
+        nums[8] = -111;
+        nums[9] = 0;
+         min = max = nums[0];
+
+
+         for(int i = 0; i < 10; i ++) {
+             if (nums [i] < min) min = nums[i];
+             if (nums [i] > max) max = nums[i];
+
+         }
+          System.out.println("min и max: " + min + " " + max);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
